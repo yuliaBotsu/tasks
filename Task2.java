5d716702e24a1ff5cc0f101003807d0874ccb3de
yuliaBotsu <87329019+yuliaBotsu@users.noreply.github.com>
2021-07-14 23:17:08 +0300

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,4 +1,35 @@
 import java.util.*;
+///////////
+
+
+
+
+class Task2
+{
+    
+    static int findMinDiff(int[] arr, int n)
+    {
+        int diff = Integer.MAX_VALUE;
+     
+        for (int i=0; i<n-1; i++)
+            for (int j=i+1; j<n; j++)
+                if (Math.abs((arr[i] - arr[j]) )< diff)
+                    diff = Math.abs((arr[i] - arr[j])); 
+        return diff;
+    }
+ 
+   
+    public static void main(String[] args)
+    {
+        int arr[] = new int[]{1, 5, 3, 19, 18, 25};
+        System.out.println("Minimum difference is "+
+                              findMinDiff(arr, arr.length));
+     
+    }
+}
+
+
+///////////////////////////
 
 public class Task2 {
     public static void main(String[] args) {
